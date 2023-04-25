9cd3ff111ad051a00538b2a1baf1e39064f4b83b
Abbyessien <essienabigail@gmail.com>
2023-04-25 16:22:56 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	_putchar('P');
+	_putchar('U');
+	_putchar('T');
+	_putchar('C');
+	_putchar('H');
+	_putchar('A');
+	_putchar('R');
+	_putchar('\n');
+
+	return (0);
+}
