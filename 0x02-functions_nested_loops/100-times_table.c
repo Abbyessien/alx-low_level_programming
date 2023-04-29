adfe90d22e707171fcc1a05478615cb992e9f99e
Abbyessien <essienabigail@gmail.com>
2023-04-29 15:34:40 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,41 +9,30 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = i * j;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
+	for (i = 0; i <= n; i++)
+	{
+	_putchar(48);
+	for (j = 0; j <= n; j++)
+	{
+	_putchar(44);
+	_putchar(32);
+	k = i * j;
+	if (k <= 99)
+	_putchar(32);
+	if (k <= 9)
+	_putchar(32);
+	if (k >= 100)
+	{
+	_putchar((k / 100) + '0');
+	_putchar(((k / 10)) % 10 + '0');
+	}
+	else if (k <= 99 && k >= 10)
+	{
+	_putchar((k % 10) + '0');
+	}
+	_putchar((k % 10) + '0');
+	}
+	_putchar('\n');
+	}
 	}
 }
